d039877693a5a2f9182a77be0c93eec58e9b088d
emmanuel badibanga <emmanuelbadibanga250@gmail.com>
2024-02-15 14:26:01 +0100

    Second commit

---
+++ test.py
@@ -0,0 +1,7 @@
+def new_user():
+    confirm = "N"
+    while confirm != "Y":
+        username = input("Indiquez le nom de l'utilisateur à ajouter: ")
+        print("Utiliser username '" + username + "'? (Y/N)")
+        confirm = input().upper()
+    os.system("sudo adduser " + username)
